7275145ec8bab6b700a53e729dca4c52e665f7ba
Coletti <matheus.coletti08@gmail.com>
2023-08-16 11:36:13 -0300

    update js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function mudarTexto() {
         setTimeout(function() {
             var elementoTexto = document.getElementById("hover-text");
             elementoTexto.textContent = "Pode clicar de novo, não tenha medo, clique varias vezes, vc vai gostar juro!";
-        }, 2500);
+        }, 2500); 
     } else {
         var elementoTexto = document.getElementById("hover-text");
         var frases = [
@@ -15,7 +15,7 @@ function mudarTexto() {
             "Gostou do meu coração? ❤️",
             "Eh sério menina, EU TE AMOOOO ❤️❤️❤️",
             "vo t encher de beijinhos ❤️❤️❤️",
-            "to com sdds de vc, mas nao me esqueço de vc 🤍"
+            "to com sdds de vc, mas não me esqueço de vc 🤍"
         ];
         var indice = Math.floor(Math.random() * frases.length);
         elementoTexto.textContent = frases[indice];
