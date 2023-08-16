76da4eba29ba6b566ebc8d957529acb553df3d38
Coletti <matheus.coletti08@gmail.com>
2023-08-16 11:33:49 -0300

    update resources

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var clickedOnce = false;
+
+function mudarTexto() {
+    if (!clickedOnce) {
+        clickedOnce = true;
+        setTimeout(function() {
+            var elementoTexto = document.getElementById("hover-text");
+            elementoTexto.textContent = "Pode clicar de novo, não tenha medo, clique varias vezes, vc vai gostar juro!";
+        }, 2500);
+    } else {
+        var elementoTexto = document.getElementById("hover-text");
+        var frases = [
+            "Eu te amo! 🤍",
+            "Naty x ℭoletti = Casal PERFEITO! 🤍",
+            "Gostou do meu coração? ❤️",
+            "Eh sério menina, EU TE AMOOOO ❤️❤️❤️",
+            "vo t encher de beijinhos ❤️❤️❤️",
+            "to com sdds de vc, mas nao me esqueço de vc 🤍"
+        ];
+        var indice = Math.floor(Math.random() * frases.length);
+        elementoTexto.textContent = frases[indice];
+    }
+}
